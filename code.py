94f76cfeb8920973ede06f6700d29c2d698ee8e3
unknown <luoy94@mcmaster.ca>
2021-01-18 10:07:40 -0500

    luoy94 player

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
 def are_valid_groups(student_numbers, groups):
 	for number in student_numbers:
 		inside = False
-		for group in student_numbersgroup:
+		for group in groups:
 			if number in group:
 				inside = True			
-		if inside:
+		if not inside:
 			return False
 	return True
