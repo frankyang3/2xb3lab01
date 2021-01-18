27094450cb2b8cf439444c7e14bc183e3e2ccc67
frankyang3 <frankyang3@gmail.com>
2021-01-18 10:03:41 -0500

    franky troll2

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 def are_valid_groups(student_numbers, groups):
 	for number in student_numbers:
 		inside = False
-		for group in groups:
+		for group in student_numbersgroup:
 			if number in group:
 				inside = True			
 		if not inside:
