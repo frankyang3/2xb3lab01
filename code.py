b3434bee7f86143d056fa0881e8bf106628eb74e
Harrison Chiu <chiuh6@mcmaster.ca>
2021-01-18 09:12:30 -0500

    fixed valid_groups()

--- code.py
+++ code.py
@@ -1,7 +1,11 @@
 def are_valid_groups(student_numbers, groups) -> bool:
+	boolean = False
 	for n in student_numbers:
 		for i in groups:
-			if not n in i:
-				return False
+			if n in i:
+				boolean = True
+				
+		if not boolean:
+			return False
 				
 	return True
