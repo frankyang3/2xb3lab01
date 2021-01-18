9481f2c61bade960e913b92f818019cb2eb27677
unknown <luoy94@mcmaster.ca>
2021-01-18 10:23:26 -0500

    new functionalities

--- code.py
+++ code.py
@@ -3,9 +3,13 @@ def is_valid_groups(student_numbers, groups):
 		inside = False
 		for number in student_numbers:
 			if number in group:
-				inside = True			
+				inside = True
+		if len(group) != 2 and len(group) != 3:
+			return False
 		if not inside:
 			return False
+	if len(student_numbers) != len(set(student_numbers)):
+		return False
 	return True
 
-print(is_valid_groups([1,2,3,4], [[1,2], [3], [4]]))
+print(is_valid_groups([1,2,3,4,4], [[1,2], [3,1], [4,1]]))
