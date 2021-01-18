c031fb6bcea619c3e8122bb79734ebe7bde9d38d
frankyang3 <frankyang3@gmail.com>
2021-01-18 09:52:33 -0500

    merged last person's files

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def are_valid_groups(student_numbers, groups):
 	
 	for number in student_numbers:
@@ -11,17 +10,3 @@ def are_valid_groups(student_numbers, groups):
 			return False
 				
 	return True
-=======
-def are_valid_groups(student_numbers, groups) -> bool:
-
-        for number in student_numbers:
-                inside = False
-                for group in groups:
-                        if number in group:
-                                inside = True
-
-                if not inside:
-                        return False
-
-        return True
->>>>>>> 2e0a74a3fa0481d880be714013847a435af489a2
